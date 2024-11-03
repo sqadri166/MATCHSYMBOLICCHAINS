a5d2926e7e755fa1d26441182ac7c9cb2ad6e7e0
= <=>
2024-11-03 12:41:02 -0500

    Reinforement Analysis

--- IReinforementLearning.cs
+++ IReinforementLearning.cs
@@ -1,18 +1,51 @@
-using OpenCV.Net;
+using Tensorflow;
+
+
+
+public enum ReinforcmentType
+{
+    Autonomous,
+    Drive ,
+    Pick,
+    Cut,
+    Clean,
+    Bite,
+    MeasureForME,
+
+    TypeForme ,
+    ReplyEmailForMe,
+    SenseObjectsForMe,
+
+    CameraDetectionCapacity
+
+
+
+}
 
 interface IReinforcementLearning 
 {
 
+       
+ 
        ReinforceDataAnalytics GenerateAnalyticsFromData(ModelData[] modelData , LearningAgent learningAgent );
        ReinforceDataAnalytics GenerateFilteredAnalytics(ModelData[] modelData , LearningAgent learningAgent);
-
+       
 }
 
 public class LearningAgent
 {
      public required ActionPerformed[] Rewards {get;set;}
-     
+     public Step SetNextStep {get;set;}
 
+
+}
+
+public class Step
+{
+   public int StepID {get;set;}
+   public string StepDescription {get;set;}
+   public ModelData StepInputs {get;set;}
+   public ModelData StepOutputs {get;set;}  
 
 }
 
@@ -21,7 +54,29 @@ public class ActionPerformed
     public required ModelData Action {get;set;}
     public decimal RewardPercentage {get;set;}
 
+    public decimal RegressionCalulation {get;set;}
+
+     public ActionPerformed(ModelData modelData)
+     {
+           this.Action= modelData;
+
+     }  
+
+    public decimal PerformCalculationForAction (ModelData modelData, ReinforcmentType type) {
+
+      /// <summary>
+      /// implementatiuon required 
+      /// </summary>
+        return  this.RegressionCalulation;
+
+    }
+
+
 }
+
+
+
+
 
 public class ReinforceDataAnalytics
 {
